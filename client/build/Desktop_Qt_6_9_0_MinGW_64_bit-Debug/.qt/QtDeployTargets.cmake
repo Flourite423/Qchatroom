7db89ec38fd6bac90415a15bef30c6dbb1404b63
leitianci <1006503427@qq.com>
2025-05-18 00:49:55 +0800

    初始化仓库

---
+++ client/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_client_FILE D:/qt_project/Qchatroom/client/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/client.exe)
+set(__QT_DEPLOY_TARGET_client_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_client_RUNTIME_DLLS D:/Program/QT/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Program/QT/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Program/QT/6.9.0/mingw_64/bin/Qt6Core.dll)
