4ffccdc1faf13541256200c701c90227fc3369ce
leitianci <1006503427@qq.com>
2025-05-19 10:29:27 +0800

    停止跟踪构建目录

--- server/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_server_FILE D:/qt_project/Qchatroom/server/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/server.exe)
-set(__QT_DEPLOY_TARGET_server_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_server_RUNTIME_DLLS D:/Program/QT/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Program/QT/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Program/QT/6.9.0/mingw_64/bin/Qt6Core.dll)
